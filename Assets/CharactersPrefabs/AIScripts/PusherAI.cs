c7f9721e0106f306fe3c65828b5a598ce6aacd53
ITheCon <cksmith@net-ex.no>
2021-03-20 22:47:53 +0100

    Added AI for pusher and tick

---
+++ Assets/CharactersPrefabs/AIScripts/PusherAI.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PusherAI : MonoBehaviour
+{
+
+    public Transform goal;
+    Vector3 start;
+    Vector3 PlayerLocation;
+
+    //float distanceToGoal;
+
+    float updatePath;
+
+    //int updateTime = 5;
+    float movementSpeed = 1;
+
+    //bool running = false;
+    bool charging = false;
+    //bool turn = false;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+
+    void Start()
+    {
+        updatePath = 0;
+        start = transform.position;
+        //distanceToGoal = 100;
+
+
+        agent = GetComponentInParent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // Only update the
+        //agent = GetComponent<NavMeshAgent>();
+        if (updatePath <= 0)
+        {
+            
+            if (charging)
+            {
+                //anim.Play("run");
+                charging = false;
+                agent.isStopped = false;
+                agent.speed = movementSpeed*11;
+                agent.destination = PlayerLocation;
+                updatePath = 4;
+            }
+            else
+            {
+                agent.speed = 0;
+                agent.isStopped = true;
+                //anim.Play("walk");
+                // Checks the distance to the goal and turns the 
+                //if (distanceToGoal < 10.0f)
+                //{
+                //    turn = !turn;
+                //}
+            }
+
+        }
+        else
+        {
+            updatePath = updatePath - 1*Time.deltaTime;
+        }
+        animator.SetFloat("Speed", agent.speed);
+        animator.SetBool("Charging", charging);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Check within trigger radius for the player 
+        if (other.gameObject.layer == 8 || other.gameObject.layer == 11)
+        {
+            if (updatePath <= 0 && charging == false)
+            {
+                charging = true;
+                PlayerLocation = other.gameObject.transform.position;
+                agent.speed = 0.1f;
+                agent.isStopped = false;
+                updatePath = 1.4f;
+            }
+            if (charging)
+            {
+                PlayerLocation = other.gameObject.transform.position;
+                agent.destination = PlayerLocation;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // If player is gone, resume normal path planning
+        //if (other.gameObject.layer == 8 || other.gameObject.layer == 11)
+        //{
+        //    running = false;
+        //}
+    }
+
+}
+
+
---
+++ Assets/CharactersPrefabs/AIScripts/TickAI.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class TickAI : MonoBehaviour
+{
+
+    public Transform goal;
+    Vector3 start;
+    Vector3 PlayerLocation;
+
+    //float distanceToGoal;
+
+    int updatePath;
+
+    //int updateTime = 5;
+    float movementSpeed = 1;
+
+    bool running = false;
+    //bool turn = false;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+
+    void Start()
+    {
+        updatePath = 0;
+        start = transform.position;
+        //distanceToGoal = 100;
+
+
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        // Only update the path planning of the penguin every couple of frames to slow down the penguins movement
+        agent = GetComponent<NavMeshAgent>();
+        if (updatePath <= 0)
+        {
+            // For when the penguin is fleeing from the player snowball instead of normal path planning
+            if (running)
+            {
+                //anim.Play("run");
+                agent.speed = movementSpeed * 2f;
+                agent.destination = PlayerLocation;
+            }
+            else
+            {
+                agent.speed = 0;
+                //anim.Play("walk");
+                // Checks the distance to the goal and turns the penguin to walk back towards its start position once it gets close enough
+                //if (distanceToGoal < 10.0f)
+                //{
+                //    turn = !turn;
+                //}
+            }
+
+        }
+        else
+        {
+            updatePath = updatePath - 1;
+        }
+        animator.SetFloat("Speed", agent.speed);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Check within trigger radius for the player snowball or falling rock, then run away
+        if (other.gameObject.layer == 8 || other.gameObject.layer == 11)
+        {
+            if (updatePath <= 0)
+            {
+                running = true;
+                PlayerLocation = other.gameObject.transform.position;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // If snowball or rock is gone, resume normal path planning
+        if (other.gameObject.layer == 8 || other.gameObject.layer == 11)
+        {
+            running = false;
+        }
+    }
+
+}
+
+
