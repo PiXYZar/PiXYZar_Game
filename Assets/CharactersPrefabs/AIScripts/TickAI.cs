4699e1377c02451a8ac1c4d068058be30ff3cfc2
ITheCon <cksmith@net-ex.no>
2021-03-24 14:15:32 +0100

    Tick AI fixed, but won't interact with player, starter for BlockShooter added too.

--- Assets/CharactersPrefabs/AIScripts/TickAI.cs
+++ Assets/CharactersPrefabs/AIScripts/TickAI.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 using UnityEngine.AI;
 public class TickAI : MonoBehaviour
 {
-
+    // The position that the tick will move towards
     public Transform goal;
-    Vector3 start;
+
+    //Vector3 start;
     Vector3 PlayerLocation;
 
     //float distanceToGoal;
 
+    //This is a counter variable that determines when the tick should update its path planning. 1 = 1 second. 
     float updatePath;
 
-    //int updateTime = 5;
     float movementSpeed = 2;
 
     float attackDistance = 10.0f;
 
+    //Booleans for determining animation and current action
     bool running = false;
     bool attacking = false;
     bool attached = false;
@@ -30,9 +32,8 @@ public class TickAI : MonoBehaviour
     void Start()
     {
         updatePath = 0;
-        start = transform.position;
+        //start = transform.position;
         //distanceToGoal = 100;
-
 
         agent = GetComponentInParent<NavMeshAgent>();
         animator = GetComponent<Animator>();
@@ -40,21 +41,23 @@ public class TickAI : MonoBehaviour
 
     private void Update()
     {
-        // Only update the path planning of the penguin every couple of frames to slow down the penguins movement
-        if (updatePath <= 0 && !agent.isStopped)
+        // Only update the path planning of 
+        if (updatePath <= 0 && !agent.isStopped && !attached)
         {
-            gameObject.GetComponentInParent<Rigidbody>().isKinematic = false;
+
             // For when the penguin is fleeing from the player snowball instead of normal path planning
             if (running)
             {
                 //anim.Play("run");
-             
+
                 agent.speed = movementSpeed;
                 agent.destination = PlayerLocation;
+
             }
             else
             {
                 agent.speed = 0;
+
                 //anim.Play("walk");
                 // Checks the distance to the goal and turns the penguin to walk back towards its start position once it gets close enough
                 //if (distanceToGoal < 10.0f)
@@ -64,9 +67,19 @@ public class TickAI : MonoBehaviour
             }
 
         }
+        else if (updatePath <= 0 && attached)
+        {
+            attached = false;
+            agent.enabled = true;
+            agent.isStopped = false;
+            //gameObject.GetComponent<Collider>().enabled = true;
+            gameObject.transform.parent.parent = null;
+            gameObject.GetComponentInParent<Rigidbody>().isKinematic = true;
+        }
         else
         {
-            updatePath = updatePath - 1;
+            // -1 every second
+            updatePath = updatePath - 1 * Time.deltaTime;
         }
         animator.SetFloat("Speed", agent.speed);
         animator.SetBool("Attacking", attacking);
@@ -78,33 +91,23 @@ public class TickAI : MonoBehaviour
         //Check if its the player
         if (other.gameObject.layer == 8)
         {
+            Debug.Log("attached");
             attached = true;
             running = false;
             attacking = false;
-            gameObject.transform.parent.parent = other.gameObject.transform;
             agent.speed = 0;
             agent.isStopped = true;
             agent.enabled = false;
-            gameObject.GetComponentInParent<Rigidbody>().isKinematic = true;
-            gameObject.GetComponent<Collider>().enabled = false;
-        }
-    }
-
-    private void OnCollisionExit(Collision other)
-    {
-        //Check if its the player
-        if (other.gameObject.layer == 8)
-        {
-            //attached = false;
-            //agent.enabled = true;
-            //gameObject.GetComponentInParent<Rigidbody>().isKinematic = true;
+            updatePath = 6;
+            gameObject.transform.parent.parent = other.gameObject.transform.parent;
+            //gameObject.GetComponent<Collider>().enabled = false;
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        // Check within trigger radius for the player snowball or falling rock, then run away
-        if (other.gameObject.layer == 8 || other.gameObject.layer == 11)
+        // Check if player within trigger radius
+        if (other.gameObject.layer == 8)
         {
             if (updatePath <= 0)
             {
@@ -116,17 +119,20 @@ public class TickAI : MonoBehaviour
 
                     if (Mathf.Abs((PlayerLocation.x + PlayerLocation.z) - (transform.position.x + transform.position.z)) <= attackDistance)
                     {
+                        running = false;
                         if (!attacking)
                         {
                             attacking = true;
                             agent.speed = 0;
+                            agent.destination = PlayerLocation;
                             updatePath = 0.15f;
                         }
                         else
                         {
-                            agent.speed = movementSpeed * 7;
+                            agent.speed = movementSpeed * 10;
+                            attacking = false;
                             updatePath = 4f;
-                            gameObject.GetComponentInParent<Rigidbody>().isKinematic = true;
+                            //gameObject.GetComponentInParent<Rigidbody>().isKinematic = true;
                         }
                     }
                     else
@@ -141,10 +147,12 @@ public class TickAI : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        // If snowball or rock is gone, resume normal path planning
-        if (other.gameObject.layer == 8 || other.gameObject.layer == 11)
+        // Check if player has left trigger area
+        if (other.gameObject.layer == 8)
         {
             running = false;
+            attacking = false;
+            attached = false;
         }
     }
 
