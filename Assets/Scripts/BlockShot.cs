94966132aa681fbd76e8310d8637b2e0058da052
Monteiro <48806912+APMMonteiro@users.noreply.github.com>
2021-03-25 12:38:17 +0000

    falling platforms and pew pew changes

--- Assets/Scripts/BlockShot.cs
+++ Assets/Scripts/BlockShot.cs
@@ -30,13 +30,11 @@ public class BlockShot : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player") 
+        if (false) //collision.gameObject.tag == "Player"
         {
-            //collision.gameObject.GetComponent<PlayerCombatStuff>().takeDamage(calculateDamage());
             Debug.Log("Collided with player");
-            collision.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * power + transform.up * power/2, ForceMode.Impulse);
-            // TODO try acceleration, impulse and velocity change https://docs.unity3d.com/ScriptReference/Rigidbody.AddForce.html
-            Destroy(gameObject);
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * power + transform.up * power, ForceMode.VelocityChange);
         }
+        Destroy(gameObject);
     }
 }
