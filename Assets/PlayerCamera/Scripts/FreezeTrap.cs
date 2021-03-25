d7af6c8461996ed933bdf30322a26654cac9d979
valeriofranchi <v.franchi@hotmail.com>
2021-03-25 18:12:01 +0000

    added freeze trap prefab

---
+++ Assets/PlayerCamera/Scripts/FreezeTrap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeTrap : MonoBehaviour
+{
+    public float freezeTime = 5.0f;
+
+    private float _timer;
+    private bool _isFrozen;
+
+    public bool IsFrozen { get { return _isFrozen; } set { _isFrozen = value; } }
+
+    void Start()
+    {
+        _timer = 0.0f;
+        _isFrozen = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (_isFrozen)
+        {
+            _timer += Time.deltaTime;
+
+            if (_timer > freezeTime)
+                _isFrozen = false;
+        }
+    }
+}
