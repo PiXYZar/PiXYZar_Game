ec761c6122265b232c3ec6abd51a6fffb9e64a78
valeriofranchi <v.franchi@hotmail.com>
2021-03-25 18:41:41 +0000

    added invisible platform

---
+++ Assets/PlayerCamera/Scripts/InvisiblePlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisiblePlatform : MonoBehaviour
+{
+    private Renderer _renderer;
+
+    void Awake()
+    {
+        _renderer = transform.Find("Model").GetComponent<Renderer>();
+        _renderer.enabled = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Bullet")
+        {
+            _renderer.enabled = true;
+        }
+    }
+}
