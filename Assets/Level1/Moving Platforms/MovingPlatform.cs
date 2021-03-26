15fd2f25cea5437ce456cc2af1669cdc0822d1e2
Monteiro <48806912+APMMonteiro@users.noreply.github.com>
2021-03-26 21:56:42 +0000

    platform is now resettable

--- Assets/Level1/Moving Platforms/MovingPlatform.cs
+++ Assets/Level1/Moving Platforms/MovingPlatform.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    //Vector3 startpos;
-    //Vector3 endpos;
     public float speed = 0.5f;
+    [Header("End position/rotation")]
+    public Vector3 endp = new Vector3(0, 0, 18f);
+    public Vector3 endrV = new Vector3(0, 90f, 0);
+
     float phase = 0f;
     float phaseDir = 1f;
     Vector3 zeros = new Vector3(0, 0, 0);
-    public Vector3 endp = new Vector3(0, 0, 18f);
-    public Vector3 endrV = new Vector3(0, 90f, 0);
+    Vector3 origP;
     Quaternion origR;
     Quaternion endr;
     Vector3 amountp;
@@ -20,14 +21,16 @@ public class MovingPlatform : MonoBehaviour
     Quaternion displacer;
     Vector3 lastDisplacep = new Vector3(0, 0, 0);
     
+    [Header("State toggles")]
     public bool isActive = false;
     public bool falling = false;
     public bool resetting = false;
-    float m = 10;
+    float fall_speed = 10;
 
     Rigidbody rb;
 
     void Start() {
+        origP = transform.position;
         origR = transform.rotation;
         endr = origR * Quaternion.Euler(endrV);
 
@@ -51,7 +54,11 @@ public class MovingPlatform : MonoBehaviour
     {
         if (resetting) 
         {
-            
+            phaseDir = -1;
+            if (phase <= 0)
+            {
+                resetting = false;
+            }
         }
         displacep = Vector3.Lerp(zeros, endp, phase);
         displacer = Quaternion.Lerp(origR, endr, phase);
@@ -76,11 +83,18 @@ public class MovingPlatform : MonoBehaviour
 
     void Fall()
     {
-        m += 30 * Time.deltaTime;
-        transform.position -= transform.up * m * Time.deltaTime;
+        fall_speed += 30 * Time.deltaTime;
+        transform.position -= transform.up * fall_speed * Time.deltaTime;
         if (transform.position.y < -100)
         {
-            Destroy(gameObject);
+            falling = false;
         }
+    }
+
+    public void Reset()
+    {
+        transform.position = origP;
+        transform.rotation = origR;
+        phase = 0;
     }
 }
