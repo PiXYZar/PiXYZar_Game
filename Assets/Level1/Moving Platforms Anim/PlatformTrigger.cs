f81be96c6c26e0fc77d4057ef115c7f453abb283
Monteiro <48806912+APMMonteiro@users.noreply.github.com>
2021-03-27 23:34:15 +0000

    added portals and small bug fixes

--- Assets/Level1/Moving Platforms Anim/PlatformTrigger.cs
+++ Assets/Level1/Moving Platforms Anim/PlatformTrigger.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class PlatformTrigger : MonoBehaviour
 {
     public GameObject thePlatfrom;
-    public GameObject thePlayer;
+
     
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player")) 
-            thePlayer.transform.parent = thePlatfrom.transform;
+            other.transform.parent = thePlatfrom.transform;
         //Debug.Log("Im in");
     }
     
     void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player")) 
-            thePlayer.transform.parent = null;
+            other.transform.parent = null;
         //Debug.Log("Im out");
     }
 }
