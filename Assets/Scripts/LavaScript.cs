65ea0e818aacf5c376a88b98f9a607453de6c366
Monteiro <48806912+APMMonteiro@users.noreply.github.com>
2021-03-28 16:42:40 +0100

    lava rises

---
+++ Assets/Scripts/LavaScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaScript : MonoBehaviour
+{
+    Vector3 orig;
+    
+    public bool rising = false;
+
+    public float raiseSpeed = 1;
+    public float speedMultiplier = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        orig = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rising)
+        {
+            transform.position += transform.up * raiseSpeed * speedMultiplier * Time.deltaTime;
+        }
+    }
+
+    private void ResetToCheckPoint()
+    {
+        transform.position = orig;
+    }
+}
