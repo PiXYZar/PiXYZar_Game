f02033af1b3ee1cc9f3ad855803c819886f0617a
ITheCon <cksmith@net-ex.no>
2021-03-28 20:43:33 +0200

    Key Prefab

---
+++ Assets/StaticObjectPrefabs/KeyCollect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCollect : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            other.gameObject.GetComponent<ThirdPersonController>().HasKey = true;
+            Destroy(gameObject);
+        }
+    }
+}
