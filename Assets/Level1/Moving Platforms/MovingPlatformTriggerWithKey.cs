c3273c88832c7b0c8e61afe9949e6e984725f1a0
eddie-LA <e21@hw.ac.uk>
2021-03-28 23:14:43 +0300

    Many small changes
    Rotating arch platform done
    Level balancing work
    Etc

---
+++ Assets/Level1/Moving Platforms/MovingPlatformTriggerWithKey.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformTriggerWithKey : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Debug.Log("player in trig area");
+            if (other.gameObject.GetComponent<ThirdPersonController>().HasKey)
+            {
+                //Debug.Log("has key");
+                //other.transform.parent = transform.parent;
+                transform.parent.GetComponent<MovingPlatform>().goingtoend = true;
+            }
+        }
+    }
+    /*
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+            transform.parent.GetComponent<MovingPlatform>().isActive = false;
+        }
+    }
+    */
+}
+
