b65d706850a8902a0d04e95130d02db4f17dbcf3
Monteiro <48806912+APMMonteiro@users.noreply.github.com>
2021-03-28 22:41:36 +0100

    onlychild

---
+++ Assets/Level1/Moving Platforms/OnlyChildPlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyChildPlayerScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = transform.parent;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
+    }
+}
