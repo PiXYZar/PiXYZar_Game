652fa8c487f99a196ee3b2b53df7a7810087fe13
eddie-LA <e21@hw.ac.uk>
2021-03-28 23:24:48 +0300

    Fixed falling platforms

---
+++ Assets/Level1/Moving Platforms/FallingPlatformTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatformTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = transform.parent;
+            transform.parent.GetComponent<MovingPlatform>().falling = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+            //transform.parent.GetComponent<MovingPlatform>().isActive = false;
+        }
+    }
+}
