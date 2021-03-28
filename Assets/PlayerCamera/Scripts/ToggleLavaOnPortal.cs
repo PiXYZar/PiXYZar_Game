ea79792d96eb41e4bb302b01cb06ca4c568cdbd5
Monteiro <48806912+APMMonteiro@users.noreply.github.com>
2021-03-28 17:09:15 +0100

    lava on portals

---
+++ Assets/PlayerCamera/Scripts/ToggleLavaOnPortal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLavaOnPortal : MonoBehaviour
+{
+    Transform planes;
+    GameObject lava;
+    // Start is called before the first frame update
+    void Start()
+    {
+        planes = transform.Find("Planes");
+        lava = GameObject.FindWithTag("Lava");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        planes.gameObject.SetActive(false);
+        if (lava.transform.position.y > planes.position.y)
+        {
+            planes.gameObject.SetActive(true);
+        }
+    }
+}
