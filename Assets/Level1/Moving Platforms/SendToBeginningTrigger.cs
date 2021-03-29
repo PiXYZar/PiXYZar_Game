4cfcf535f22f4e187db175f04fed9331d31fdfdc
Monteiro <48806912+APMMonteiro@users.noreply.github.com>
2021-03-29 14:40:40 +0100

    it's the final aaaaa

---
+++ Assets/Level1/Moving Platforms/SendToBeginningTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendToBeginningTrigger : MonoBehaviour
+{
+    public Transform platform;
+    
+    private void OnTriggerEnter(Collider other) {
+        platform.GetComponent<MovingPlatform>().goingtobeginning = true;
+    }
+}
---
+++ Assets/Level1/Moving Platforms/SendToEndTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendToEndTrigger : MonoBehaviour
+{
+    public Transform platform;
+    
+    private void OnTriggerEnter(Collider other) {
+        platform.GetComponent<MovingPlatform>().goingtoend = true;
+    }
+}
